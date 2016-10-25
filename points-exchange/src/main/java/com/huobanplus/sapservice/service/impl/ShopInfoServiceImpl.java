59480f74a85bacd38cc4e64d4d3ba6f1c052bf78
allan <lqc_huli@163.com>
2016-10-25 15:55:42 +0800

    xxxx

--- points-exchange/src/main/java/com/huobanplus/sapservice/service/impl/ShopInfoServiceImpl.java
+++ points-exchange/src/main/java/com/huobanplus/sapservice/service/impl/ShopInfoServiceImpl.java
@@ -30,7 +30,7 @@ public class ShopInfoServiceImpl implements ShopInfoService {
     public void initShopInfo() throws IOException {
         ShopInfo info = shopInfoRepository.findOne(1);
         if(info == null){// 未初始化过数据，则初始化，否则不初始化
-            InputStream fin = ShopInfoServiceImpl.class.getResourceAsStream("/guitaiinfo.xlsx");
+            InputStream fin = ShopInfoServiceImpl.class.getResourceAsStream("/shop_info_10.17.xlsx");
             XSSFWorkbook workbook= new XSSFWorkbook(fin);
             for(int i=0;i<workbook.getNumberOfSheets();i++){
                 XSSFSheet sheet = workbook.getSheetAt(i);
